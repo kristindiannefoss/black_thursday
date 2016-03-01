a577f6029fe2b3f54dbcddb4d4585357290bbddd
Sal Espinosa <sespinos@gmail.com>
2016-02-29 20:48:06 -0700

    Minor updates

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,18 +1,20 @@
 require 'pry'
 
 class MerchantRepository
-  def initialize
-  end
-
-  def all
-  end
-
-  def find_by_id
+  def initialize(merchants)
     #returns an array of all known Merchant instances
   end
 
-  def find_by_name
+  def all
+    #returns an array of all known Merchant instances
+  end
+
+  def find_by_id
     #returns either nil or an instance of Merchant
+  end
+
+  def find_by_name
+    #returns either nil or an instance of Merchant having done a case insensitive search
   end
 
   def find_by_all_name
