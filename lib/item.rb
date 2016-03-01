66a48b1cf2af5545537776920722e3d3ae005feb
Kris Foss <kris.foss@gmail.com>
2016-02-29 18:40:45 -0700

    added item.rb

---
+++ lib/item.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+class MerchantRepository
+  def initialize
+  end
+
+  def id
+    #returns the integer id of the item
+  end
+
+  def name
+    #returns the name of the item
+  end
+
+  def description
+    #- returns the description of the item
+  end
+
+  def unit_price
+    #- returns the price of the item formatted as a BigDecimal
+  end
+
+  def created_at
+    # - returns a Time instance for the date the item was first created
+  end
+
+  def updated_at
+    # - returns a Time instance for the date the item was last modified
+  end
+
+  def merchant_id
+    # - returns the integer merchant id of the item
+  end
+
+  def unit_price_to_dollars
+    # - returns the price of the item in dollars formatted as a Float
+  end
+end
