7177ecc077908248dc63d1a927491443c7b102a3
Sal Espinosa <sespinos@gmail.com>
2016-03-02 16:34:50 -0700

    Correct standard deviation and create high item count

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -14,6 +14,14 @@ class SalesAnalyst
 
     Math.sqrt(@sales_engine.merchants.all.map do |merchant|
       ((merchant.items.count) - av_it)**2
-    end.reduce(:+)/2)
+    end.reduce(:+)/@sales_engine.merchants.count)
+  end
+
+  def merchants_with_high_item_count
+    minimum_count = average_items_per_merchant + average_items_per_merchant_standard_deviation
+
+    @sales_engine.merchants.all.select do |merchant|
+      merchant.items.count > minimum_count
+    end
   end
 end
