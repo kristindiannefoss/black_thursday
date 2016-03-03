d893775a285e16459c51274f34b87702f43183f4
Sal Espinosa <sespinos@gmail.com>
2016-03-03 11:02:02 -0700

    Initial commit for Invoice, related tests, and related stub

---
+++ lib/invoice.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+
+class Invoice
+  attr_reader :id, :customer_id, :merchant_id, :status, :created_at, :updated_at
+
+  def initialize(args)
+    @id            = args[:id].to_i
+    @customer_id   = args[:customer_id].to_i
+    @merchant_id   = args[:merchant_id].to_i
+    @status        = args[:status]
+    @created_at    = Time.parse(args[:created_at])
+    @updated_at    = Time.parse(args[:updated_at])
+  end
+end
