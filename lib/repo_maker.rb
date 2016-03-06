cf033c01b2f947f886334d5f966c4b4d2fab9b21
Sal Espinosa <sespinos@gmail.com>
2016-03-06 12:55:38 -0700

    Remove comments and update some initialize references

--- lib/repo_maker.rb
+++ lib/repo_maker.rb
@@ -13,80 +13,13 @@ class RepoMaker
   end
 
   def create_repos(object_arrays)
-    { items: ItemRepository.new(args[:items_array], sales_engine),
-      merchants: MerchantRepository.new(args[:merchants_array], sales_engine),
-      invoices: InvoiceRepository.new(args[:invoices_array], sales_engine),
-      invoice_items: InvoiceItemRepository.new(args[:invoice_items_array], sales_engine),
-      transactions: TransactionRepository.new(args[:transaction_array], sales_engine),
-      customers: CustomerRepository.new(args[:customers_array], sales_engine)
+    { items: ItemRepository.new(object_arrays[:items_array], sales_engine),
+      merchants: MerchantRepository.new(object_arrays[:merchants_array], sales_engine),
+      invoices: InvoiceRepository.new(object_arrays[:invoices_array], sales_engine),
+      invoice_items: InvoiceItemRepository.new(object_arrays[:invoice_items_array], sales_engine),
+      transactions: TransactionRepository.new(object_arrays[:transaction_array], sales_engine),
+      customers: CustomerRepository.new(object_arrays[:customers_array], sales_engine)
     }
   end
-
-  # def inject_repositories(merchants_repo, items_repo, invoices_repo, invoice_items_repo, transactions_repo, customer_repo)
-  #   inject_merchants_repo(merchants_repo, items_repo, invoices_repo)
-  #   inject_items_repo(items_repo, merchants_repo)
-  #   inject_invoices_repo(invoices_repo, merchants_repo, invoice_items_repo, items_repo, transactions_repo, customer_repo)
-  #   inject_transactions_repo(transactions_repo, invoices_repo)
-  #   inject_merchant_customers(merchants_repo, invoices_repo, customer_repo)
-  # end
-  #
-  # def inject_merchants_repo(merchants_repo, items_repo, invoices_repo)
-  #   merchants_repo.all.each do |merchant|
-  #     merchant.items     = items_repo.find_all_by_merchant_id(merchant.id)
-  #     merchant.invoices  = invoices_repo.find_all_by_merchant_id(merchant.id)
-  #   end
-  # end
-  #
-  # def inject_merchant_customers(merchants_repo, invoices_repo, customer_repo)
-  #   merchants_repo.all.each do |merchant|
-  #     merchant_invoices = invoices_repo.find_all_by_merchant_id(merchant.id)
-  #
-  #     merchant.customers = merchant_invoices.map do |invoice|
-  #       customer_repo.find_by_id(invoice.customer_id)
-  #     end.uniq
-  #   end
-  # end
-  #
-  # def inject_items_repo(items_repo, merchants_repo)
-  #   items_repo.all.each do |item|
-  #     item.merchant = merchants_repo.find_by_id(item.merchant_id)
-  #   end
-  # end
-  #
-  # def inject_invoices_repo(invoices_repo, merchants_repo, invoice_items_repo, items_repo, transaction_repo, customer_repo)
-  #   invoices_repo.all.each do |invoice|
-  #     invoice.merchant    = inject_invoice_merchants(invoice, merchants_repo)
-  #     invoice.items       = inject_invoice_items(invoice_items_repo, invoice, items_repo)
-  #     invoice.transactions = inject_invoice_transactions(invoice, transaction_repo)
-  #     invoice.customer    = inject_invoice_customer(invoice, customer_repo)
-  #   end
-  # end
-  #
-  # def inject_invoice_merchants(invoice, merchants_repo)
-  #   merchants_repo.find_by_id(invoice.merchant_id)
-  # end
-  #
-  # def inject_invoice_items(invoice_items_repo, invoice, items_repo)
-  #   invoice_item_ids  = invoice_items_repo.find_all_by_invoice_id(invoice.id).map do |invoice_item|
-  #     invoice_item.item_id
-  #   end
-  #   invoice_item_ids.map do |item_id|
-  #     items_repo.find_by_id(item_id)
-  #   end
-  # end
-  #
-  # def inject_invoice_transactions(invoice, transactions_repo)
-  #   transactions_repo.find_all_by_invoice_id(invoice.id)
-  # end
-  #
-  # def inject_invoice_customer(invoice, customer_repo)
-  #   customer_repo.find_by_id(invoice.customer_id)
-  # end
-  #
-  # def inject_transactions_repo(transactions_repo, invoices_repo)
-  #   transactions_repo.all.each do |transaction|
-  #     transaction.invoice = invoices_repo.find_by_id(transaction.invoice_id)
-  #   end
-  # end
 
 end
--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -4,7 +4,6 @@ require 'bigdecimal'
 require_relative 'object_maker'
 require_relative 'repo_maker'
 
-
 class SalesEngine
   attr_reader :merchants,
               :items,
@@ -14,12 +13,14 @@ class SalesEngine
               :customers
 
   def initialize(repos = {})
-    @merchants     = repos[:merchants]
-    @items         = repos[:items]
-    @invoices      = repos[:invoices]
-    @invoice_items = repos[:invoice_items]
-    @transactions  = repos[:transactions]
-    @customers     = repos[:customers]
+    if repos != {}
+      @merchants     = repos[:merchants]
+      @items         = repos[:items]
+      @invoices      = repos[:invoices]
+      @invoice_items = repos[:invoice_items]
+      @transactions  = repos[:transactions]
+      @customers     = repos[:customers]
+    end
   end
 
   def self.from_csv(args)
@@ -34,15 +35,5 @@ class SalesEngine
 
     return se
   end
-
-
-
-  # def self.from_csv(args)
-  #   items_array, merchants_array, invoices_array, invoice_items_array, transactions_array, customers_array = read_all_csv(args)
-  #   items_repo, merchants_repo, invoices_repo, invoice_items_repo, transactions_repo, customer_repo = create_repos(items_array, merchants_array, invoices_array, invoice_items_array, transactions_array, customers_array)
-  #   inject_repositories(merchants_repo, items_repo, invoices_repo, invoice_items_repo, transactions_repo, customer_repo)
-  #
-  #   SalesEngine.new(merchants_repo, items_repo, invoices_repo, invoice_items_repo, transactions_repo, customer_repo)
-  # end
 
 end
