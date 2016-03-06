4b8bf58e1845d9b709c2d0295abfe04db21bec4c
Sal Espinosa <sespinos@gmail.com>
2016-03-05 18:38:36 -0700

    Start refactor of iteration_3

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -15,20 +15,20 @@ require_relative 'transaction_repository'
 class SalesEngine
   attr_reader :merchants, :items, :invoices, :invoice_items, :transactions
 
-  def initialize(merchants = nil, items = nil, invoices = nil, invoice_items = nil, transactions = nil)
-    @merchants     = merchants
-    @items         = items
-    @invoices      = invoices
-    @invoice_items = invoice_items
-    @transactions  = transactions
+  def initialize(args)
+    @merchants     = args[:merchants]
+    @items         = args[:items]
+    @invoices      = args[:invoices]
+    @invoice_items = args[:invoice_items]
+    @transactions  = args[:transactions]
   end
 
   def self.from_csv(args)
-    items_array, merchants_array, invoices_array, invoice_items_array, transactions_array = read_all_csv(args)
-    items_repo, merchants_repo, invoices_repo, invoice_items_repo, transactions_repo = create_repos(items_array, merchants_array, invoices_array, invoice_items_array, transactions_array)
-    inject_repositories(merchants_repo, items_repo, invoices_repo, invoice_items_repo, transactions_repo)
+    csv_arrays = read_all_csv(args)
+    repos = create_repos(csv_arrays)
+    inject_repositories(repos)
 
-    SalesEngine.new(merchants_repo, items_repo, invoices_repo, invoice_items_repo, transactions_repo)
+    SalesEngine.new(repos)
   end
 
   def self.read_all_csv(args)
@@ -50,43 +50,44 @@ class SalesEngine
     CSV.open(csv_location, headers: true, header_converters: :symbol)
   end
 
-  def self.create_repos(items_array, merchants_array, invoices_array, invoice_items_array, transaction_array)
-    [ItemRepository.new(items_array),
-      MerchantRepository.new(merchants_array),
-      InvoiceRepository.new(invoices_array),
-      InvoiceItemRepository.new(invoice_items_array),
-      TransactionRepository.new(transaction_array)]
+  def self.create_repos(csv_arrays)
+    [ ItemRepository.new(csv_arrays[:items]),
+      MerchantRepository.new(csv_arrays[:merchants]),
+      InvoiceRepository.new(csv_arrays[:invoices]),
+      InvoiceItemRepository.new(csv_arrays[:invoice_items]),
+      TransactionRepository.new(csv_arrays[:transaction])
+    ]
   end
 
-  def self.inject_repositories(merchants_repo, items_repo, invoices_repo, invoice_items_repo, transactions_repo)
-    inject_merchants_repo(merchants_repo, items_repo, invoices_repo)
-    inject_items_repo(items_repo, merchants_repo)
-    inject_invoices_repo(invoices_repo, merchants_repo, invoice_items_repo)
-    inject_transactions_repo(transactions_repo, invoices_repo)
+  def self.inject_repositories(repos)
+    inject_merchants_repo(repos)
+    inject_items_repo(repos)
+    inject_invoices_repo(repos)
+    inject_transactions_repo(repos)
   end
 
-  def self.inject_merchants_repo(merchants_repo, items_repo, invoices_repo)
-    merchants_repo.all.each do |merchant|
-      merchant.items = items_repo.find_all_by_merchant_id(merchant.id)
-      merchant.invoices = invoices_repo.find_all_by_merchant_id(merchant.id)
+  def self.inject_merchants_repo(repos)
+    repos[:merchants].all.each do |merchant|
+      merchant.items = repos[:items].find_all_by_merchant_id(merchant.id)
+      merchant.invoices = repos[:invoices].find_all_by_merchant_id(merchant.id)
     end
   end
 
-  def self.inject_items_repo(items_repo, merchants_repo)
-    items_repo.all.each do |item|
-      item.merchant = merchants_repo.find_by_id(item.merchant_id)
+  def self.inject_items_repo(repos)
+    repos[:items].all.each do |item|
+      item.merchant = repos[:merchants].find_by_id(item.merchant_id)
     end
   end
 
-  def self.inject_invoices_repo(invoices_repo, merchants_repo, invoice_items_repo)
-    invoices_repo.all.each do |invoice|
-      invoice.merchant = merchants_repo.find_by_id(invoice.merchant_id)
-      invoice.items    = invoice_items_repo.find_all_by_invoice_id(invoice.id)
+  def self.inject_invoices_repo(repos)
+    repos[:invoices].all.each do |invoice|
+      invoice.merchant = repos[:merchants].find_by_id(invoice.merchant_id)
+      invoice.items    = repos[:invoice_items].find_all_by_invoice_id(invoice.id)
     end
   end
 
-  def self.inject_transactions_repo(transactions_repo, invoices_repo)
-    transactions_repo.all.each do |transaction|
+  def self.inject_transactions_repo(repos)
+    repos[:transactions].all.each do |transaction|
       transaction.invoice = invoices_repo.find_by_id(transaction.invoice_id)
     end
   end
