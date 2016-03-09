d9fe8f43a6c9f50644969b29c73285e942eca292
Sal Espinosa <sespinos@gmail.com>
2016-03-09 13:53:34 -0700

    Adjust line length for rake sanitation

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -27,36 +27,36 @@ class InvoiceRepository
   end
 
   def find_by_id(id)
-    invoices.detect { |invoice_object| invoice_object.id == id }
+    invoices.detect { |invoice| invoice.id == id }
   end
 
   def find_all_by_customer_id(customer_id)
-    invoices.select do |invoice_object|
-      invoice_object.customer_id == customer_id
+    invoices.select do |invoice|
+      invoice.customer_id == customer_id
     end
   end
 
   def find_all_by_merchant_id(merchant_id)
-    invoices.select do |invoice_object|
-      invoice_object.merchant_id == merchant_id
+    invoices.select do |invoice|
+      invoice.merchant_id == merchant_id
     end
   end
 
   def find_all_by_status(status)
-    invoices.select do |invoice_object|
-      invoice_object.status == status
+    invoices.select do |invoice|
+      invoice.status == status
     end
   end
 
   def find_all_by_date(created_at)
-    invoices.select do |invoice_object|
-      invoice_object.created_at.strftime("%Y%m%d") == created_at.strftime("%Y%m%d")
+    invoices.select do |invoice|
+      invoice.created_at.strftime("%Y%m%d") == created_at.strftime("%Y%m%d")
     end
   end
 
   def find_all_pending
-    invoices.select do |invoice_object|
-      invoice_object.is_pending?
+    invoices.select do |invoice|
+      invoice.is_pending?
     end
   end
 
